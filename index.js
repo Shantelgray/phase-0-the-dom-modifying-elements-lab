189fba4bcc9a646471ea6407a77d67837c72ae67
ShanteL Gray <Shantelgray24@gmail.com>
2025-03-01 17:23:12 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector("main")
+console.log(main)
+main.remove()
+
+const newHeader = document.querySelector("h1");
+newHeader.id = ("victory")
+
+newHeader.innerHTML = "Shantel is the champion"
